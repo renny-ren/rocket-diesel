b7955d1f406ae5b63bde7ebe2dc1a08db229cbb2
Renny Ren <rennyallen@hotmail.com>
2023-02-07 16:02:00 +0800

    app config

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 #[macro_use]
 extern crate rocket;
-// use diesel::{prelude::*, table, Insertable, Queryable};
 use rocket::{fairing::AdHoc, serde::json::Json, State};
-// use rocket_sync_db_pools::database;
 use serde::{Deserialize, Serialize};
 
-// fn main() {
-//   println!("Hello, world!");
-// }
+#[derive(Serialize, Deserialize)]
+struct BlogPost {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    name: String,
+    age: u8,
+}
 
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build();
     rocket
-        .mount("/", routes![index])
-        .mount("/blog-posts", routes![get_random_blog_post, get_blog_post])
+        .attach(AdHoc::config::<Config>())
+        .mount("/", routes![index, get_config])
+        .mount(
+            "/blog-posts",
+            routes![
+                get_random_blog_post,
+                get_blog_post,
+                get_all_blog_posts,
+                create_blog_post
+            ],
+        )
 }
 
 #[get("/")]
@@ -42,10 +59,30 @@ fn get_blog_post(id: i32) -> Json<BlogPost> {
     })
 }
 
-#[derive(Serialize, Deserialize)]
-struct BlogPost {
-    id: i32,
-    title: String,
-    body: String,
-    published: bool,
+#[get("/")]
+fn get_all_blog_posts() -> Json<Vec<BlogPost>> {
+    Json(vec![
+        BlogPost {
+            id: 0,
+            title: "My First Title".to_string(),
+            body: "My First Body".to_string(),
+            published: true,
+        },
+        BlogPost {
+            id: 1,
+            title: "My Second Title".to_string(),
+            body: "My Second Body".to_string(),
+            published: true,
+        },
+    ])
+}
+
+#[get("/config")]
+fn get_config(config: &State<Config>) -> String {
+    format!("Hello, {} years old named {}!", config.age, config.name)
+}
+
+#[post("/", data = "<blog_post>")]
+fn create_blog_post(blog_post: Json<BlogPost>) -> Json<BlogPost> {
+    blog_post
 }
